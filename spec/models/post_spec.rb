610158b58c054c24542c9ba357d593cd3bc28ddf
tatiana.mojica <tatiana.mojica@bustle.com>
2019-03-17 21:04:16 +0000

    Automatically backed up by Learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Post do
   
   it 'can be created'
-    post = Post.create! (title: "My title", decsription: "The post description")
+    post = Post.create!(title: "My title", decsription: "The post description")
   
     expect(post).to be_valid
   end
