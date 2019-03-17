33056648a4d38167f44c23a74658c423d4a1821d
tatiana.mojica <tatiana.mojica@bustle.com>
2019-03-17 20:48:23 +0000

    Automatically backed up by Learn

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+ 
+describe Post do
+  
+  it 'can be created'
+    post = Post.create! (title: "My title", decsription: "The post description")
+  
+    expect(post).to be_valid
+  end
+ 
+end
